e09fc8c450709f4b2c203241ad0f52bc589dc774
ShahriarNayeem <shahriarnayeem58@gmail.com>
2021-05-30 12:49:36 +0600

    style added

--- new-workflow-react/.storybook/preview.js
+++ new-workflow-react/.storybook/preview.js
@@ -1,10 +1,10 @@
-import 'antd/dist/antd.min.css';
+import '../src/assets/scss/index.scss';
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
